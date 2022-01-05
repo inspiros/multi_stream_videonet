87a0cae841f3c7efa89b81e6268eb77514820dbd
inspiros <inspiros.tran@gmail.com>
2022-01-06 05:04:02 +0700

    add host check for run_train_afosr_rgb.sh

--- scripts/run_train_afosr_rgb.sh
+++ scripts/run_train_afosr_rgb.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 cd "${0%/*}/.." || exit
 
-if (($(hostname) == "Server2")); then
-  echo "Running on Server2"
+host=$(hostname)
+echo "[sh] Running on $host"
+
+if ((host == "Server2")); then
   video_dir=/mnt/disk3/datasets/afosr2022/data
   train_annotation_file=/mnt/disk3/datasets/afosr2022/train.txt
   test_annotation_file=/mnt/disk3/datasets/afosr2022/val.txt
   device=cuda:1
-elif (($(hostname) == "hungvuong")); then
-  echo "Running on hungvuong"
+elif ((host == "hungvuong")); then
   video_dir=/ext_data2/comvis/datasets/afosr2022/data
   train_annotation_file=/ext_data2/comvis/datasets/afosr2022/train.txt
   test_annotation_file=/ext_data2/comvis/datasets/afosr2022/val.txt
   device=cuda:3
 else
-  echo "Host not supported yet"
+  echo "Host not supported!"
   exit 1
 fi
 
